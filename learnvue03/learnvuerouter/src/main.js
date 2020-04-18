e5b0d34cc37eacb2ca985fea0881d5b3e6b3aa1b
1549664680 <1549664680@qq.com>
2020-04-18 15:40:41 +0800

    router

--- learnvue03/learnvuerouter/src/main.js
+++ learnvue03/learnvuerouter/src/main.js
@@ -4,8 +4,21 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// Vue.prototype.test = function () {
+//   console.log('test');
+// }
+// Vue.prototype.name = "coderwhy"
+
 new Vue({
   el: '#app',
   router,
   render: h => h(App)
 })
+
+// console.log(router);
+
+// const obj = {
+//   name: 'why'
+// }
+//
+// Object.defineProperty(obj, 'age', 18)
--- learnvue03/learnvuerouter/src/router/index.js
+++ learnvue03/learnvuerouter/src/router/index.js
@@ -2,8 +2,17 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 
-import Home from '../components/Home'
-import About from '../components/About'
+// import Home from '../components/Home'
+// import About from '../components/About'
+// import User from '../components/User'
+
+const Home = () => import('../components/Home')
+const HomeNews = () => import('../components/HomeNews')
+const HomeMessage = () => import('../components/HomeMessage')
+
+const About = () => import('../components/About')
+const User = () => import('../components/User')
+const Profile = () => import('../components/Profile')
 
 // 1.通过Vue.use(插件), 安装插件
 Vue.use(VueRouter)
@@ -17,11 +26,49 @@ const routes = [
   },
   {
     path: '/home',
-    component: Home
+    component: Home,
+    meta: {
+      title: '首页'
+    },
+    children: [
+      // {
+      //   path: '',
+      //   redirect: 'news'
+      // },
+      {
+        path: 'news',
+        component: HomeNews
+      },
+      {
+        path: 'message',
+        component: HomeMessage
+      }
+    ]
   },
   {
     path: '/about',
-    component: About
+    component: About,
+    meta: {
+      title: '关于'
+    },
+    beforeEnter: (to, from, next) => {
+      // console.log('about beforeEnter');
+      next()
+    }
+  },
+  {
+    path: '/user/:id',
+    component: User,
+    meta: {
+      title: '用户'
+    },
+  },
+  {
+    path: '/profile',
+    component: Profile,
+    meta: {
+      title: '档案'
+    },
   }
 ]
 const router = new VueRouter({
@@ -29,6 +76,20 @@ const router = new VueRouter({
   routes,
   mode: 'history',
   linkActiveClass: 'active'
+})
+
+// 前置守卫(guard)
+router.beforeEach((to, from, next) => {
+  // 从from跳转到to
+  document.title = to.matched[0].meta.title
+  // console.log(to);
+  // console.log('++++');
+  next()
+})
+
+// 后置钩子(hook)
+router.afterEach((to, from) => {
+  // console.log('----');
 })
 
 // 3.将router对象传入到Vue实例
